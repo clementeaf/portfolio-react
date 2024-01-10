c0c901cf7e38b4e311819d332763de4e4c876d7b
clementeaf <ext_carriagada@sbpay.cl>
2024-01-10 18:22:11 -0300

    updateState message in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,7 @@ const buttonsRow = bottomMenu.map((content) => <Button children={content} />)
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-[#d5d5d5]/60">
-      <div className="flex flex-col h-full w-full px-5 pt-10 pb-5">
-        <Header/>
-        <div className="flex flex-col w-full h-full border-b border-black pb-4 mt-2">
-          <div className="flex h-[700px] justify-start items-start pt-14">
-            {/* <Projects /> */}
-            <Home />
-          </div>
-          <div className="flex justify-between w-[375px] fixed bottom-9 right-5">
-            {buttonsRow}
-          </div>
-        </div>
-      </div>
+      Update in progres. Please comeback later
     </div>
   )
 }
